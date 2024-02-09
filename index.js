a1cfcf69e88fae008fab5ce4083d79e1ac7d7843
NiteshRajput26 <148003383+NiteshRajput26@users.noreply.github.com>
2024-02-09 11:25:42 +0000

    index

---
+++ index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app.js"
+
+// ()() iffe 1.() simple method  2.() run the method
+(async () =>{
+    try{
+   await  mongoose.connect("mongodb://localhost:27017/gitsetup")
+   console.log("DB CONNECTED");
+
+   const onListening = () =>{
+    console.log("Listening on PORT 5000");
+   }
+// onListening provide refrence only not a method
+   app.listen(5000, onListening)
+    }catch (error){
+        console.error("error:" ,error);
+        throw err;
+    }
+})()
